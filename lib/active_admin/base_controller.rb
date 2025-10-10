cda41b093e81cb0abbe1bc17552545b5777efeb3
emmanu-snapmint <emmanu.varghese@snapmintfin.com>
2025-10-10 19:08:14 +0530

    Update base_controller.rb

--- lib/active_admin/base_controller.rb
+++ lib/active_admin/base_controller.rb
@@ -5,7 +5,7 @@ require "active_admin/base_controller/menu"
 module ActiveAdmin
   # BaseController for ActiveAdmin.
   # It implements ActiveAdmin controllers core features.
-  class BaseController < ::InheritedResources::Base
+  class BaseController < ActionController::Base
     helper ::ActiveAdmin::ViewHelpers
 
     layout :determine_active_admin_layout
