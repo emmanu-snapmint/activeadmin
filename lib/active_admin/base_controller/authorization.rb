bd6a02a9cc81b04b89f89701349b280adebd1d82
emmanu-snapmint <emmanu.varghese@snapmintfin.com>
2025-10-10 19:12:57 +0530

    Update authorization.rb

--- lib/active_admin/base_controller/authorization.rb
+++ lib/active_admin/base_controller/authorization.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module ActiveAdmin
-  class BaseController < ::InheritedResources::Base
+  class BaseController < ActionController::Base
     module Authorization
       extend ActiveSupport::Concern
 
