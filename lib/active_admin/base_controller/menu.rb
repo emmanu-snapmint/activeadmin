cb3879b96ea10eb453e1a02d2cfab1fa41022538
emmanu-snapmint <emmanu.varghese@snapmintfin.com>
2025-10-10 19:25:02 +0530

    Update menu.rb

--- lib/active_admin/base_controller/menu.rb
+++ lib/active_admin/base_controller/menu.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module ActiveAdmin
-  class BaseController < ::InheritedResources::Base
+  class BaseController < ActionController::Base
     module Menu
       extend ActiveSupport::Concern
 
